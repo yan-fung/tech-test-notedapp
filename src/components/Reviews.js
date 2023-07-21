d6b8d90f6a067128137df0a2ac060358b18f8744
Yan Fung <fungmingyan@hotmail.com>
2023-07-21 19:46:48 +0100

    Implemented function to fetch data from firebase, used the map method to extract value for each review and passed them as props to the ReviewsCards component

---
+++ src/components/Reviews.js
@@ -0,0 +1,55 @@
+import { collection, getDocs } from "firebase/firestore";
+import { db } from "../firebase";
+import { useEffect } from "react";
+import ReviewsCards from "./ReviewsCards";
+
+const Reviews = ({ reviews, setReviews, isLoading, setIsLoading }) => {
+  const fetchReview = async () => {
+    setIsLoading(true);
+
+    try {
+      await getDocs(collection(db, "reviews")).then((querySnapshot) => {
+        const newData = querySnapshot.docs.map((doc) => ({
+          ...doc.data(),
+          id: doc.id,
+        }));
+        setReviews(newData);
+        console.log(reviews, newData);
+      });
+    } catch (error) {
+      console.log("Error fetching data:", error);
+    } finally {
+      setIsLoading(false);
+    }
+  };
+
+  useEffect(() => {
+    fetchReview();
+  }, []);
+
+  // Get the first three reviews only
+  const firstThreeReviews = reviews.slice(0, 3);
+
+  return (
+    <div className="review-content" data-testid="review__content">
+      {isLoading ? (
+        <div>Loading...</div>
+      ) : (
+        firstThreeReviews.map((review) => (
+          <div key={review.id} className="item">
+            <ReviewsCards
+              body={review.body}
+              reviewId={review.id}
+              date={review.date}
+              name={review.name}
+              rating={review.rating}
+              title={review.title}
+            />
+          </div>
+        ))
+      )}
+    </div>
+  );
+};
+
+export default Reviews;
